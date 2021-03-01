4f295b0006b2713fd10e6f8e853ca0ffc4d929db
OranGeNaL <39371893+OranGeNaL@users.noreply.github.com>
2021-03-01 15:56:38 +1000

    Сделал макет приложения

---
+++ course/6_Semester/FileManager/FileInView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class FileInView
+    {
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public string Size { get; private set; }
+
+        public FileInView() { }
+        public FileInView(string name)
+        {
+            Name = name;
+            Type = "file";
+            Size = "0 B";
+        }
+        public FileInView(string name, string type, string size)
+        {
+            Name = name;
+            Type = type;
+            Size = size;
+        }
+    }
+}
---
+++ course/6_Semester/FileManager/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace FileManager
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<FileInView> filesInView = new List<FileInView>();
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            foreach(var i in Directory.EnumerateFiles(@"C:\"))
+            {
+                filesInView.Add(new FileInView(i));
+            }
+            /*for (int i = 0; i < 50; i++)
+                filesInView.Add(new FileInView("Файл", "File", "150G"));*/
+            filesView.ItemsSource = filesInView;
+        }
+    }
+}
